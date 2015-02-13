b0a41c61226022e2616be67d7aec82445f032c03
clare trembath <claretrembath@ymail.com>
2015-02-13 15:08:22 +0000

    changed logo, changed colours

--- js/script.js
+++ js/script.js
@@ -7,12 +7,26 @@ $(document).ready(function(){
 			$("header").css("top", 0);
 		}
 		else {
-			$("header").css("top", -200);
+			$("header").css("top", -300);
 		}
 		
 		$("body").toggleClass("oh", title > 40);
 		$("body").toggleClass("hi", title > 190);
 		$("body").toggleClass("im", title > 450);
 		$("body").toggleClass("clre", title > 750);
-	})
+		$("body").toggleClass("pictures", title > 1380);
+		$("body").toggleClass("mail", title > 2110);
+
+		// $("header").fadeOut( title > 300);
+
+		var w = 600 - t/4;
+
+		w= Math.max(200, w);
+		w=Math.min(400,w);
+
+		$("header img").css("width", w);
+			
+			
+		})
+
 })
