7929fe6473abf52c7df0b2a813cecd62ed96d865
clare trembath <claretrembath@ymail.com>
2015-02-13 15:23:14 +0000

    colors

--- js/script.js
+++ js/script.js
@@ -15,7 +15,8 @@ $(document).ready(function(){
 		$("body").toggleClass("im", title > 450);
 		$("body").toggleClass("clre", title > 750);
 		$("body").toggleClass("pictures", title > 1380);
-		$("body").toggleClass("mail", title > 2110);
+		$("body").toggleClass("git", title > 1700);
+		$("body").toggleClass("email", title > 2100);
 
 		// $("header").fadeOut( title > 300);
 
