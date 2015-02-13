b8cf102c22ebc921f56ca95ba1905cca4e17a2ea
clare trembath <claretrembath@ymail.com>
2015-02-13 13:40:58 +0000

    scroll changes color using toggleClass also, font family added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	$(document).on("scroll", function(){
-		var title=$(document).scroll();
-		$("div#distance").html(title+"pixels down")
+		var title=$(document).scrollTop();
+
+		$("div#distance").html(title+"pixels down");
 		if(title > 30) {
 			$("header").css("top", 0);
 		}
@@ -9,5 +10,9 @@ $(document).ready(function(){
 			$("header").css("top", -200);
 		}
 		
+		$("body").toggleClass("oh", title > 40);
+		$("body").toggleClass("hi", title > 190);
+		$("body").toggleClass("im", title > 450);
+		$("body").toggleClass("clre", title > 750);
 	})
 })
